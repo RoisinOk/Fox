c131e73df9e080533d3d1be4f93ded5b19266dbc
RoisinOk <roisin_ok@hotmail.com>
2015-07-16 09:58:59 +0100

    Made timeline overlap. Shortened timeline code.

--- main.js
+++ main.js
@@ -2,28 +2,16 @@
  * Created by roisinokeeffe on 15/07/2015.
  */
 
+//Declare a new timeline. Make it repeat twice (3 times total), with a delay of 1 second
 
-var tl = new TimelineMax({repeat:2, repeatDelay:1});
+var foxTimeline = new TimelineMax({repeat:2, repeatDelay:1});
 
-tl.add( TweenLite.to("#head", 1, {
+foxTimeline.to("#head", 1, {
     rotation    :   "-5",
     top         :   "5%",
-}) );
+}) ;
 
-tl.add( TweenLite.to("#head",.1, {opacity:1}) );
-tl.add( TweenLite.to("#head2",.1, {opacity:0}) );
-tl.add( TweenLite.to("#head",.1, { rotation:"5", top:"5%",}) );
-
-tl.add( TweenLite.from("#tail", 1, {
-    top             :   "-15%",
-    left            :   "-5%",
-    rotation        :   "90",
-    transformOrigin :   "27% 100%",
-    ease            :   Back.easeOut.config(2), y: 0
-
-}) );
-
-tl.add( TweenLite.to("#circle", 1, {
+foxTimeline.to("#circle", 2, {
     height          :   450,
     width           :   450,
     top             :   "1%",
@@ -31,8 +19,21 @@ tl.add( TweenLite.to("#circle", 1, {
     left           :   "53%",
     ease            :   Back.easeOut.config(2), y: 0
 
-}) );
+});
+
+foxTimeline.to("#head",0, {opacity:1}, "-=1.75") ;
+foxTimeline.to("#head2",0, {opacity:0}, "-=1.75") ;
+foxTimeline.to("#head",.5, { rotation:"5", top:"5%",}, "-=1.5");
 
 
+
+foxTimeline.from("#tail", 1, {
+    top             :   "-15%",
+    left            :   "-5%",
+    rotation        :   "90",
+    transformOrigin :   "27% 100%",
+    ease            :   Back.easeOut.config(2), y: 0
+
+}, "-=1.75");
 
 
